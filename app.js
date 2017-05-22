bfe1506263d46283f61a43b33b10b7c09ab2cff2
ahmadaizoki <aizoki.ahmad@gmail.com>
2017-05-22 10:45:03 +0200

    fb

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.get('/webhook/', function (req, res) {
 app.post('/webhook/', function (req, res) {
 	var data = req.body;
 	console.log(JSON.stringify(data));
-	parseExcel(xlsxtojson);
+	parseExcel();
 
 
 
